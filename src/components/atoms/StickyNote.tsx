c7d1178c2917f58243e6de6eb6fab597882ba84f
AKSHAY-PV-8 <akshaypv19@gmail.com>
2026-03-07 12:52:52 +0530

    feat: implemented Card UI with textArea

--- src/components/atoms/StickyNote.tsx
+++ src/components/atoms/StickyNote.tsx
@@ -1,8 +1,15 @@
 
 const StickyNote = () => {
+    const currentDate = new Date
+    const month = currentDate.toLocaleString("en-US", {month: "short"});
+    const date = currentDate.getDate();
+    const year = currentDate.getFullYear();
+
   return (
-    <div>
-      
+    <div className={`h-65 bg-amber-200 rounded-3xl border flex flex-col  justify-center gap-2 p-6 text-[#9aa3b2] hover:scale-101 `}>
+      <div className="font-roboto font-bold">{month} {date}, {year}</div>
+      <textarea name="" id="" className="focus: outline-none font-caveat text-2xl h-40 text-black"></textarea>
+      <div className="font-roboto font-bold">0/300</div>
     </div>
   )
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,15 @@
+import StickyNote from "../components/atoms/StickyNote"
 import NavBar from "../components/molecules/NavBar"
 
 const HomePage = () => {
   return (
     <div>
       <NavBar/>
+      <StickyNote/>
     </div>
   )
 }
 
 export default HomePage
+
+
